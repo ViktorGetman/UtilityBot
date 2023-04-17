00970b438804ff7a7e4bd96c4cf91341e7da3699
ViktorGetman <123113442+ViktorGetman@users.noreply.github.com>
2023-04-17 23:52:14 +0300

    Добавьте файлы проекта.

---
+++ UtilityBot/Controllers/Class1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using VoiceTexterBot.Services;
+
+namespace VoiceTexterBot.Controllers
+{
+    public class Class1
+    {
+        private readonly IStorage _memoryStorage; // Добавим это
+        private readonly ITelegramBotClient _telegramClient;
+        
+
+        public Class1 (ITelegramBotClient telegramBotClient, IStorage memoryStorage)
+        {
+            _telegramClient = telegramBotClient;
+            _memoryStorage = memoryStorage; // и это
+        }
+
+        public async Task Handle(Message message, CancellationToken ct)
+        {          
+
+            string userLanguageCode = _memoryStorage.GetSession(message.Chat.Id).LanguageCode; // Здесь получим язык из сессии пользователя
+
+            if(userLanguageCode == "ru")
+                
+            {
+                await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Длина сообщения: {message.Text.Length} знаков");
+            }
+
+        }
+    }
+}
---
+++ UtilityBot/Controllers/InlineKeyboardController.cs
@@ -0,0 +1,41 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using VoiceTexterBot.Services;
+
+namespace VoiceTexterBot.Controllers
+{
+    public class InlineKeyboardController
+    {
+        private readonly IStorage _memoryStorage;
+        private readonly ITelegramBotClient _telegramClient;
+
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
+        {
+            _telegramClient = telegramBotClient;
+            _memoryStorage = memoryStorage;
+        }
+
+        public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
+        {
+            if (callbackQuery?.Data == null)
+                return;
+
+            // Обновление пользовательской сессии новыми данными
+            _memoryStorage.GetSession(callbackQuery.From.Id).LanguageCode = callbackQuery.Data;
+
+            // Генерим информационное сообщение
+            string languageText = callbackQuery.Data switch
+            {
+                "ru" => " Подсчет символов",
+                "en" => " Сложение чисел",
+                _ => String.Empty
+            };
+
+            // Отправляем в ответ уведомление о выборе
+            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id,
+                $"<b>Вы выбрали - {languageText}.{Environment.NewLine}</b>" +
+                $"{Environment.NewLine}Можно поменять в главном меню.", cancellationToken: ct, parseMode: ParseMode.Html);
+        }
+    }
+}
